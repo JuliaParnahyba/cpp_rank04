37c13eae44a06de8f22d4136cd5cb6c8c95b5159
JuliaParnahyba <prog.jump@gmail.com>
2025-07-02 21:08:36 -0300

    feat(module00-ex01): add Contact class header file

---
+++ Developing/Module00/ex01/inputs/Contact.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/02 20:44:02 by jparnahy          #+#    #+#             */
+/*   Updated: 2025/07/02 20:58:10 by jparnahy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* --------- INCLUDE GUARD --------- */
+// if not defined, define this.
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+/* --------- HEADERS --------- */
+#include <string> // for std::string
+#include <iostream> // for std::cout
+
+/* --------- CLASSES --------- */
+/**
+ * @class Contact
+ * @brief Representa um único contato com seus dados pessoais.
+ */
+class Contact {
+private:
+    // Atributos privados que armazenam cada campo do contato.
+    std::string _firstName;
+    std::string _lastName;
+    std::string _nickname;
+    std::string _phoneNumber;
+    std::string _darckSecret;
+
+public:
+    /**
+     * @brief Define o valor de um campo do contato.
+     * @param fieldName Nome do campo ("firstName", "lastName", etc.)
+     * @param value Valor a ser atribuído.
+     */
+    void setField(std::string fieldName, std::string value);
+    
+    /**
+     * @brief Retorna o valor de um campo específico.
+     * @param fieldName Nome do campo desejado.
+     * @return Valor do campo.
+     */
+    string getField(std::string fieldName) const;
+
+    /**
+     * @brief Exibe o resumo do contato na tabela de listagem.
+     * @param index Índice do contato no PhoneBook.
+     */
+    void displaySummary(int index) const;
+
+    /**
+     * @brief Exibe todos os detalhes do contato, campo por campo.
+     */
+    void displayDetails() const;
+}
+
+#endif
+
