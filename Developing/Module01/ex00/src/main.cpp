83b20af1cd3cbe3ed888e96c8ece75db73362ba1
JuliaParnahyba <prog.jump@gmail.com>
2025-08-12 20:42:20 -0300

    feat(ex00): scaffold project structure and Makefile

---
+++ Developing/Module01/ex00/src/main.cpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/12 19:22:29 by jparnahy          #+#    #+#             */
+/*   Updated: 2025/08/12 20:40:52 by jparnahy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+// temporary sanity check, will be replaced in next steps
+int main() {
+    std::cout << "Compiled as expected" << std::endl;
+    return 0;
+}
