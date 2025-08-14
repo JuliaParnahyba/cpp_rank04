d40966909238b901cf0a149665273d01f62ffeb4
JuliaParnahyba <prog.jump@gmail.com>
2025-08-14 20:50:00 -0300

    refactor(ex01): minimal main (iterate N only), remove temporary 'Tester'.

--- Developing/Module01/ex01/src/main.cpp
+++ Developing/Module01/ex01/src/main.cpp
@@ -6,23 +6,20 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 19:22:29 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/08/14 20:38:24 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/08/14 20:49:26 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int main() {
-    Zombie z;               // default
-    z.setName("Tester");    // pós new[] será parecido
-    z.announce();           // "Tester: BraiiiiiiinnnzzzZ..."
-
     const int N = 5;
     Zombie *h = zombieHorde(N, "HordeGuy");
+
     if (h) {
         for (int i = 0; i < N; i++)
             h->announce();
-        delete[] h;
+        delete[] h; // correct pair for new[]
     }
         
     return 0;
