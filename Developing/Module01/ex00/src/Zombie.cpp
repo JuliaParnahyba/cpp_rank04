a171d66ad00dca1670e22b7b2c6796c25ee77acd
JuliaParnahyba <prog.jump@gmail.com>
2025-08-14 12:41:03 -0300

    refactor(ex00): finalize minimal main and remove debug logs

--- Developing/Module01/ex00/src/Zombie.cpp
+++ Developing/Module01/ex00/src/Zombie.cpp
@@ -6,26 +6,20 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 19:23:40 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/08/14 11:50:58 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/08/14 12:34:04 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie() : _name("nameless") {
-    // friendly name: nameless
-    std::cout << "unused " << this->_name << std::endl;
+Zombie::Zombie() : _name("nameless") { /*body clean*/ }
+
+Zombie::Zombie(const std::string &name) : _name(name) { /*body clean*/ }
+
+void    Zombie::announce() const {
+    std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
 
 Zombie::~Zombie() {
     std::cout << this->_name << " is destroyed" << std::endl;
-}
-
-Zombie::Zombie(const std::string &name) : _name(name) {
-    // corpo vazio
-    std::cout << "input name: " << this->_name << std::endl;
-}
-
-void    Zombie::announce() const {
-    std::cout << this->_name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- Developing/Module01/ex00/src/main.cpp
+++ Developing/Module01/ex00/src/main.cpp
@@ -6,50 +6,24 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 19:22:29 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/08/14 12:18:53 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/08/14 12:37:45 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 int main() {
-    std::cout << "[BOOT] starting default-ctor initialization test\n";
-
-    // ---------- SCOPE A: default-constructed ----------
-    std::cout << "[A] begin scope\n";
-    {
-        Zombie z1;
-        z1.announce();
-        std::cout << "[A] mid scope (z exists here)\n";
-    }
-    std::cout << "[A] end scope (z was destroyed above)\n";
-
-    // ---------- SCOPE B: name-constructed -------------
-    std::cout << "[B] begin scope\n";
-    {
-        Zombie z2("Alice");
-        z2.announce();
-        std::cout << "[B] mid scope (z exists here)\n";
-    }
-    std::cout << "[B] end scope (z was destroyed above)\n";
-
-    // ---------- SCOPE C: newZombie (heap) -------------
-    std::cout << "[C] begin scope\n";
+    // ---------- SCOPE newZombie (heap) -------------
     {
         Zombie *p = newZombie("Heapster");
         p->announce();
-        std::cout << "[C] mid scope\n";
         delete p;
     }
-    std::cout << "[C] end scope\n";
-
-    // ---------- SCOPE D: randomChump (stack na função) -------------
-    std::cout << "[D] begin scope\n";
+    
+    // ---------- SCOPE randomChump (stack on function) -------------
     {
         randomChump("Ephemeral");
     }
-    std::cout << "[D] end scope\n";
-
-    std::cout << "[DONE] default-ctor test complete\n";
+    
     return 0;
 }
