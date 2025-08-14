9db650e714c5eacfd60ef8a4b4b70a59096ee8de
JuliaParnahyba <prog.jump@gmail.com>
2025-08-14 20:41:43 -0300

    feat(ex01): implement zombieHorde(N, name) using new[] and post-construction naming

--- Developing/Module01/ex01/src/main.cpp
+++ Developing/Module01/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/12 19:22:29 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/08/14 19:59:42 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/08/14 20:38:24 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@ int main() {
     Zombie z;               // default
     z.setName("Tester");    // pós new[] será parecido
     z.announce();           // "Tester: BraiiiiiiinnnzzzZ..."
-    
+
+    const int N = 5;
+    Zombie *h = zombieHorde(N, "HordeGuy");
+    if (h) {
+        for (int i = 0; i < N; i++)
+            h->announce();
+        delete[] h;
+    }
+        
     return 0;
 }
--- Developing/Module01/ex01/src/zombieHorde.cpp
+++ Developing/Module01/ex01/src/zombieHorde.cpp
@@ -6,9 +6,21 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 19:29:53 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/08/14 19:34:59 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/08/14 20:34:10 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+// Caller owns the returned array and must delete[] it.
+Zombie *zombieHorde(int N, const std::string &name) {
+    if (N <= 0)
+        return 0;   // no horde; caller must check and not use
+    
+    Zombie *z = new Zombie[N]; // default-construct N elements
+    for (int i = 0; i < N; i++) {
+        z[i].setName(name);    // post-construction init
+    }
+
+    return z;   // caller owns and must delete[] z;
+}
