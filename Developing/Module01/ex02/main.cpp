f68649d7bc8984e9e4814481edd01b6f0538ed54
JuliaParnahyba <prog.jump@gmail.com>
2025-08-18 22:00:26 -0300

    feat(ex02): declare string, pointer and reference with didactic comments;

--- Developing/Module01/ex02/main.cpp
+++ Developing/Module01/ex02/main.cpp
@@ -6,13 +6,34 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 20:49:24 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/08/18 21:45:19 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/08/18 21:53:29 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <string>
+
+/* -------------------------------------------------------------------------- */
+/*  Goal                                                                      */
+/*    Print:                                                                  */
+/*      (A) the memory addresses of:                                          */
+/*          - the string variable                                             */
+/*          - the string pointer                                              */
+/*          - the string reference                                            */
+/*      (B) the string value through:                                         */
+/*          - the variable                                                    */
+/*          - the pointer dereference                                         */
+/*          - the reference                                                   */
+/* -------------------------------------------------------------------------- */
 
 int main() {
     std::cout << "run..." << std::endl;
+
+    std::string s = "HI THIS IS BRAIN";
+    std::string *stringPTR = &s;
+    std::string &stringREF = s;
+
+    
+
     return 0;
 }
