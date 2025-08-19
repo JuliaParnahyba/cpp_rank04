176b4cf99678655162609595229c6445ff21f03f
JuliaParnahyba <prog.jump@gmail.com>
2025-08-18 21:49:22 -0300

    chore(ex02): fix .PHONY (add eclean/run/tests/tests_hell, remove debug) and add `eclean` rule to remove only the executable

--- Developing/Module01/ex02/main.cpp
+++ Developing/Module01/ex02/main.cpp
@@ -6,13 +6,13 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 20:49:24 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/08/18 21:20:32 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/08/18 21:45:19 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-// bootstrap placeholder; prints nothing for now
 int main() {
     std::cout << "run..." << std::endl;
-    return 0; }
+    return 0;
+}
