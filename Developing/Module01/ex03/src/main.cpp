fdb59cd96379707a38c21a3488112a92f4dec22d
JuliaParnahyba <prog.jump@gmail.com>
2025-08-20 23:02:14 -0300

    feat(module01-ex03): add delivery main with exact 4-line output

--- Developing/Module01/ex03/src/main.cpp
+++ Developing/Module01/ex03/src/main.cpp
@@ -6,39 +6,30 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 21:43:53 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/08/20 22:27:26 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/08/20 22:58:40 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream> // std::cout, std::endl
 #include "Weapon.hpp"
 #include "HumanA.hpp"
 #include "HumanB.hpp"
 
-
 int main() {
-    std::cout << "Its run...\n" << std::endl;
-
-    Weapon wA("crude spiked club"); // Objeto instanciado e construtor
-    std::cout << "wA is " << wA.getType() << std::endl; // o método devolve uma referência constante
-    wA.setType("some other type of club"); // invoca setType(), que passa novo valor para type
-    std::cout << "the new value of wA: " << wA.getType() << std::endl;
-
-    std::cout << std::endl;
-
-    Weapon wB("some other type of club");
-    std::cout << "wB is " << wB.getType() << std::endl;
-    wB.setType("crud spiked club");
-    std::cout << "the new value of wB: " << wB.getType() << std::endl;
-
-    std::cout << std::endl;
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanA bob("Bob", club);
+        bob.attack();
+        club.setType("some other type of club");
+        bob.attack();
+    }
+    {
+        Weapon club = Weapon("crude spiked club");
+        HumanB jim("Jim");
+        jim.setWeapon(club);
+        jim.attack();
+        club.setType("some other type of club");
+        jim.attack();
+    }
     
-    HumanA a("Julia", wA);
-    a.attack();
-
-    HumanB b("Fabi");
-    b.setWeapon(wB);
-    b.attack();
-
     return 0;
 }
