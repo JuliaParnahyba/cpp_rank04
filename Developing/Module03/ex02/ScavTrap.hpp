45ad1c1395b05a01dba008e992e6d133f277ac8e
JuliaParnahyba <prog.jump@gmail.com>
2025-10-09 22:28:18 -0300

    feat(module03/ex02): add FragTrap (OCF, 100/100/30 stats, DRY attack via base call, highFivesGuys, tests) and ignore fragtrap binary

---
+++ Developing/Module03/ex02/ScavTrap.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/09 22:12:45 by jparnahy          #+#    #+#             */
+/*   Updated: 2025/10/09 22:12:47 by jparnahy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* ---------- INCLUDE GUARD ---------- */
+// if not defined, define this.
+#ifndef SCAVTRAP_HPP    // start include guard: prevent multiple inclusion
+#define SCAVTRAP_HPP    // define guard macro once for this translation unit
+
+/* ---------- STANDARD LIBRARIES ---------- */
+#include <iostream>
+#include <string>
+
+/* --------- PROJECT HEADERS --------- */
+#include "ClapTrap.hpp"      // ClapTrap class for base class to inherit
+
+// inherit class
+class   ScavTrap : public ClapTrap {
+    public:
+        // OCF
+        ScavTrap();                                 // def. ctor
+        explicit ScavTrap(const std::string &name); // expl. ctor
+
+        ScavTrap(const ScavTrap &other);            // copy ctor
+        ScavTrap &operator=(const ScavTrap &rhs);   // copy asgn
+        ~ScavTrap();                                // dtor
+
+        // method
+        void    attack(const std::string &target);
+        void    guardGate();
+};
+
+#endif // SCAVTRAP_HPP
+// end include guard
