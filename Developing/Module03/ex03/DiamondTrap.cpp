4cedd688abeee02d37e3bd5355dbef5d7050e199
JuliaParnahyba <prog.jump@gmail.com>
2025-10-11 19:31:09 -0300

    fix(module03/ex03): initialize virtual ClapTrap in DiamondTrap copy-ctor and clean up name copy; replace comma ops with statements

--- Developing/Module03/ex03/DiamondTrap.cpp
+++ Developing/Module03/ex03/DiamondTrap.cpp
@@ -6,8 +6,47 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 18:37:03 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/10/11 18:44:08 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/10/11 19:27:34 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
+
+DiamondTrap::DiamondTrap()
+: ClapTrap("NoName_clap_name"), FragTrap("NoName"), ScavTrap("NoName") { 
+    _name = "NoName";
+    _hitPoints = 100;   // from FragTrap   
+    _energyPoints = 50; // from ScavTrap
+    _attackDamage = 30; // from FragTrap
+    std::cout << "[DiamondTrap] default-ctor: " << _name << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const std::string &name)
+: ClapTrap(name + "_clap_name"), FragTrap(name), ScavTrap(name) {
+    _name = name;       // name from own DiamondTrap
+    _hitPoints = 100;   // from FragTrap   
+    _energyPoints = 50; // from ScavTrap
+    _attackDamage = 30; // from FragTrap
+    std::cout << "[DiamondTrap] name-ctor: " << _name << std::endl;
+}
+
+DiamondTrap::DiamondTrap(const DiamondTrap &other)
+: ClapTrap(other), FragTrap(other), ScavTrap(other), _name(other._name) {
+    //_name = other._name;
+    std::cout << "[Diamond] copy-ctor: " << _name << std::endl;
+}
+
+DiamondTrap &DiamondTrap::operator=(const DiamondTrap &rhs) {
+    ScavTrap::operator=(rhs);
+    FragTrap::operator=(rhs);
+
+    if (this != &rhs)
+        _name = rhs._name;
+    std::cout << "[DiamondTrap] copy-assign: " << _name << std::endl;
+    
+    return *this;
+}
+
+DiamondTrap::~DiamondTrap() {
+    std::cout << "[DiamondTrap] dtor: " << _name << std::endl;
+}
--- Developing/Module03/ex03/main.cpp
+++ Developing/Module03/ex03/main.cpp
@@ -6,21 +6,49 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/11 18:25:05 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/10/11 18:45:41 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/10/11 19:29:22 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
 #include "FragTrap.hpp"
+#include "DiamondTrap.hpp"
 
 int main() {
     std::cout << "Its run\n" << std::endl;
     
+    std::cout << "---------------------------\n";
+    std::cout << "         SCAVTRAP\n";
     ScavTrap def;
+    std::cout << "\n";
+    std::cout << def.getName() << "'s Energy Points are " << def.getEnergyPoints() << std::endl;
+    std::cout << def.getName() << "'s Hit Points are " << def.getHitPoints() << std::endl;
+    std::cout << def.getName() << "'s Attack Damage are " << def.getAttackDamage() << std::endl;
+    std::cout << "\n";
+
+    std::cout << "---------------------------\n";
+    std::cout << "         FRAGTRAP\n";
     FragTrap s("Serena");
+    std::cout << "\n";
+    std::cout << s.getName() << "'s Energy Points are " << s.getEnergyPoints() << std::endl;
+    std::cout << s.getName() << "'s Hit Points are " << s.getHitPoints() << std::endl;
+    std::cout << s.getName() << "'s Attack Damage are " << s.getAttackDamage() << std::endl;
+    std::cout << "\n";
+
+    std::cout << "---------------------------\n";
+    std::cout << "         DIAMONDTRAP\n";
+    DiamondTrap c("Dias");
+    std::cout << "\n";
+    std::cout << c.getName() << "'s Energy Points are " << c.getEnergyPoints() << std::endl;
+    std::cout << c.getName() << "'s Hit Points are " << c.getHitPoints() << std::endl;
+    std::cout << c.getName() << "'s Attack Damage are " << c.getAttackDamage() << std::endl;
+    std::cout << "\n";
     
     std::cout << "\nIt's done\n" << std::endl;
+
+    std::cout << "---------------------------\n";
+    std::cout << "         DISTRUCTOR\n";
 
     return 0;
 }
