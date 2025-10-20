359887ae407a8523e2c72410b6a5c9850948fd54
JuliaParnahyba <prog.jump@gmail.com>
2025-10-20 12:11:36 -0300

    refactor(cpp04-ex02): check Animal abstract and polymorphism tests

--- Developing/Module04/ex02/main.cpp
+++ Developing/Module04/ex02/main.cpp
@@ -6,31 +6,35 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 19:33:22 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/10/19 20:10:57 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/10/20 12:08:55 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
 #include "Brain.hpp"
 
 int main() {
-    std::cout << "=== POLIMORPHISM + VIRTUAL DESTRUCTOR ===" << std::endl;
+    std::cout << "======= POLIMORPHISM + VIRTUAL DESTRUCTOR =======" << std::endl;
 
     // Animal a;                    // "error: cannot declare variable ‘a’ to be of abstract type ‘Animal’"
     // Animal *b = new Animal();    // "error: invalid new-expression of abstract class type ‘Animal’"
 
-    std::cout << "\n=== Calling constructor ===\n";
+    std::cout << "=== Calling constructor ===\n";
     Animal *c = new Cat();
     Animal *d = new Dog();
+
+    std::cout << "\n=== Calling method/behavior ===\n";
     c->makeSound();
     d->makeSound();
+    
+    std::cout << "\n=== Calling destructor ===\n";
     delete c;
     delete d;
-    std::cout << "\n";
 
-    std::cout << "=== Array test (no leaks, polymorphism) ===";
+    std::cout << "\n=== Array test (no leaks, polymorphism) ===";
     const int N = 6;
     Animal* zoo[N];
 
@@ -43,7 +47,7 @@ int main() {
     std::cout << "\n=== Seeing each class  ===\n";
     for (int i = 0; i < N; ++i) {
         std::cout << zoo[i]->getType() << " -> ";
-        zoo[i]->makeSound(); // polimorfismo
+        zoo[i]->makeSound(); // polymorphism
     }
 
     std::cout << "\n=== Calling destructor  ===\n";
