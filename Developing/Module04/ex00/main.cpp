78b80f14bf400305c4f0f1beafaa192ea980f513
JuliaParnahyba <prog.jump@gmail.com>
2025-10-20 10:50:10 -0300

    refactor(cpp04-ex00): update main to use slim headers and avoid UB in wrong-polymorphism demo

--- Developing/Module04/ex00/main.cpp
+++ Developing/Module04/ex00/main.cpp
@@ -6,10 +6,11 @@
 /*   By: jparnahy <jparnahy@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 21:28:30 by jparnahy          #+#    #+#             */
-/*   Updated: 2025/10/17 22:54:05 by jparnahy         ###   ########.fr       */
+/*   Updated: 2025/10/20 10:49:20 by jparnahy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "Animal.hpp"
 #include "Dog.hpp"
 #include "Cat.hpp"
@@ -19,32 +20,41 @@
 int main() {
     std::cout << "Lets known the sound of Animals\n" << std::endl;
 
-    std::cout << "=== Right polymorphism ===\n";
+    std::cout << "=== RIGHT POLYMORPHISM ===\n";
+    std::cout << "=== Constructor ===\n";
     const Animal *meta = new Animal();
     const Animal *d = new Dog();
     const Animal *c = new Cat();
 
     std::cout << "\nhas a [" << d->getType() << "]" << std::endl;
-    std::cout << "has a [" << c->getType() << "]\n" << std::endl;
+    d->makeSound();     // Dog
+    
+    std::cout << "\nhas a [" << c->getType() << "]" << std::endl;
+    c->makeSound();     // Cat
 
-    c->makeSound();
-    d->makeSound();
-    meta->makeSound();
+    std::cout << "\nhas a [" << meta->getType() << "]" << std::endl;
+    meta->makeSound();  // Animal
 
+    std::cout << "\n=== Destructor ===\n";
     delete meta;
     delete d;
     delete c;
 
-    std::cout << "\n=== Wrong polymorphism ===\n";
-    const WrongAnimal* wa = new WrongAnimal();
-    const WrongAnimal* wc = new WrongCat();
+    std::cout << "\n=== WRONG POLYMORPHISM ===\n";
+    std::cout << "=== Constructor ===\n";
+    const WrongAnimal *wa = new WrongAnimal();
+    const WrongAnimal *wc = new WrongCat();
 
-    std::cout << "\nhas a [" << wc->getType() << "]\n" << std::endl;
+    std::cout << "\nhas a [" << wc->getType() << "]" << std::endl;
     wc->makeSound(); // vai chamar WrongAnimal::makeSound() (sem virtual)
+
+    std::cout << "\nhas a [" << wa->getType() << "]" << std::endl;
     wa->makeSound();
 
+    std::cout << "\n=== Destructor ===\n";
     delete wc;
     delete wa;
 
+    std::cout << "\n=== DONE ===\n";
     return 0;
 }
